99012c2b4373a99ecc8ed5cc013b8e14174fb24d
Shubhangi <shubhidewangan208@gmail.com>
2023-03-26 02:03:13 +0530

    Stacks

---
+++ Queue.py
@@ -0,0 +1,41 @@
+class Queue:
+    def __init__(self):
+        self.item = []
+
+    def __str__(self):
+        values = [str(x) for x in self.item]
+        return ' '.join(values)
+
+    def isEmpty(self):
+        if self.item == []:
+            return True
+        else:
+            return False
+    
+    def enqueue(self,value):
+        self.item.append(value)
+        return "The element is inserted at the end od Queue"
+	
+    def dequeue(self):
+        if self.isEmpty():
+            return "There is not any element in the Queue"
+        else:
+            return self.item.pop(0)
+
+    def peek(self):
+        if self.isEmpty():
+            return "There is no such List"
+        else:
+            return self.item[0]
+
+	
+
+customQueue = Queue()
+print(customQueue.isEmpty())
+customQueue.enqueue(1)
+customQueue.enqueue(2)
+customQueue.enqueue(3)
+customQueue.enqueue(4)
+customQueue.enqueue(5)
+print(customQueue.dequeue())
+print(customQueue)
--- stacks.py
+++ stacks.py
@@ -1,120 +1,120 @@
-# class Stack:
-#     def __init__(self):
-#         self.list = []
+class Stack:
+    def __init__(self):
+        self.list = []
 
-#     def __str__(self):
-#         values = self.list.reverse()
-#         values = [str(x) for x in self.list]
-#         return '\n'.join(values)
+    def __str__(self):
+        values = self.list.reverse()
+        values = [str(x) for x in self.list]
+        return '\n'.join(values)
 
-#     # isEmpty
+    # isEmpty
 
-#     def isEmpty(self):
-#         if self.list == []:
-#             return True
-#         else:
-#             return False
+    def isEmpty(self):
+        if self.list == []:
+            return True
+        else:
+            return False
 
-#     # push
-#     def push(self,value):
-#         self.list.append(value)
-#         return "the element has been successfully inserted"
+    # push
+    def push(self,value):
+        self.list.append(value)
+        return "the element has been successfully inserted"
 
-#     # pop
-#     def pop(self):
-#         if self.isEmpty():
-#             return "There is no such List"
-#         else:
-#             return self.list.pop()
+    # pop
+    def pop(self):
+        if self.isEmpty():
+            return "There is no such List"
+        else:
+            return self.list.pop()
 
-#     # peek
-#     def peek(self):
-#         if self.isEmpty():
-#             return "There is no such List"
-#         else:
-#             return self.list[len(self.list)-1]
+    # peek
+    def peek(self):
+        if self.isEmpty():
+            return "There is no such List"
+        else:
+            return self.list[len(self.list)-1]
         
-#     # delete
-#     def delete(self):
-#         self.list = None
+    # delete
+    def delete(self):
+        self.list = None
 
-# customStack = Stack() 
-# customStack.push(1)
-# customStack.push(2)
-# customStack.push(3)
-# customStack.push(4)
-# # print(customStack)
-# # print(customStack.pop())
-# print(customStack.peek())
-# # print(customStack.delete())
+customStack = Stack() 
+customStack.push(1)
+customStack.push(2)
+customStack.push(3)
+customStack.push(4)
+# print(customStack)
+# print(customStack.pop())
+print(customStack.peek())
+# print(customStack.delete())
 
 # Stack with limit
 
-# class Stack:
-#     def __init__(self, maxSize):
-#         self.maxSize = maxSize
-#         self.list = []
+class Stack:
+    def __init__(self, maxSize):
+        self.maxSize = maxSize
+        self.list = []
 
-#     def __str__(self):
-#         values = self.list.reverse()
-#         values = [str(x) for x in self.list]
-#         return '\n'.join(values)
+    def __str__(self):
+        values = self.list.reverse()
+        values = [str(x) for x in self.list]
+        return '\n'.join(values)
 
-#     # isEmpty
+    # isEmpty
       
-#     def isEmpty(self):
-#         if self.list == []:
-#             return True
-#         else:
-#             return False
+    def isEmpty(self):
+        if self.list == []:
+            return True
+        else:
+            return False
 
-#     # isFull
+    # isFull
 
-#     def isFull(self):
-#         if len(self.list) >= self.maxSize:
-#             return True
-#         else:
-#             return False
+    def isFull(self):
+        if len(self.list) >= self.maxSize:
+            return True
+        else:
+            return False
 
-#     # push (TC-amortized)
+    # push (TC-amortized)
 
-#     def push(self,value):
-#         if self.isFull():
-#             return 'the stack is full'
-#         else:
-#             self.list.append(value)
-#             return 'the element has been successfully inserted'
+    def push(self,value):
+        if self.isFull():
+            return 'the stack is full'
+        else:
+            self.list.append(value)
+            return 'the element has been successfully inserted'
 
-#     # pop
+    # pop
 
-#     def pop(self):
-#         if self.isEmpty():
-#             return "There is no such List"
-#         else:
-#             return self.list.pop()
+    def pop(self):
+        if self.isEmpty():
+            return "There is no such List"
+        else:
+            return self.list.pop()
 
-#     # peek
+    # peek
 
-#     def peek(self):
-#         if self.isEmpty():
-#             return "There is no such List"
-#         else:
-#             return self.list[len(self.list)-1]
+    def peek(self): 
+        if self.isEmpty():
+            return "There is no such List"
+        else:
+            return self.list[len(self.list)-1]
 
-#     # delete
-#     def delete(self):
-#         self.list = None
+    # delete
+    def delete(self):
+        self.list = None
 
 
 
-# customStack = Stack(3)
-# print(customStack.isEmpty())
-# customStack.push(1)
-# customStack.push(2)
-# customStack.push(3)
-# customStack.push(6)
-# print(customStack.isFull()) 
-# print(customStack)
+customStack = Stack(3)
+print(customStack.isEmpty())
+customStack.push(1)
+customStack.push(2)
+customStack.push(3)
+customStack.push(6)
+print(customStack.isFull()) 
+print(customStack)
 
 # Operation on Stack using Linked List
 
